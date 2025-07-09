e20a1b34f95515dcc01c3d88c6e042b6388df59c
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-07-09 21:32:02 +0545

    push

---
+++ DrawingApp.java
@@ -0,0 +1,8 @@
+
+// Shape Drawing App: Create a system with an abstract class Shape, then extend it to different types of shapes (Circle, Rectangle, etc.), using polymorphism to draw them.
+
+import java.
+public class DrawingApp {
+
+
+}
